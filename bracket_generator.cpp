f194385972ba14df313b0a65403daab0a995c250
n-dett <124851780+n-dett@users.noreply.github.com>
2024-03-20 17:45:26 -0500

    First commit

---
+++ bracket_generator.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Natalie Dettmer on 3/19/24.
+//
+
+#include "bracket_generator.hpp"
+
+
+// Team constructor
+Team::Team(int seed, int zipDigits, int pantoneColor) :
+    seed(seed), zipDigits(zipDigits), pantoneColor(pantoneColor){}
+
+
+
+
+
+    // Random
+    // Team with lowest last three digits of zip code
+    // Team with highest last two digits of primary Pantone color
---
+++ bracket_generator.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Natalie Dettmer on 3/19/24.
+//
+
+#ifndef FINAL_PROJECT_BRACKET_GENERATOR_HPP
+#define FINAL_PROJECT_BRACKET_GENERATOR_HPP
+
+class Team {
+private:
+    int zipDigits;
+    int pantoneColor;
+
+public:
+    int seed;
+
+    Team(int seed, int zipDigits, int pantoneColor);
+
+
+
+};
+
+
+
+#endif //FINAL_PROJECT_BRACKET_GENERATOR_HPP
